31c68083c66daebb56f770c17088e50129afcdbc
Dylan Jay Flores <dylanjay153@gmail.com>
2023-05-11 16:00:24 -0600

    Implementacion test

---
+++ src/test/java/com/mx/consultaya/utils/RandomStringTest.java
@@ -0,0 +1,12 @@
+package com.mx.consultaya.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class RandomStringTest {
+    
+    @Test
+    void makeTest(){
+        String s = RandomString.make(2);
+    }
+    
+}
